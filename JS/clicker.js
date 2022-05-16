89d94bc488bf439d8870d3876f6c2e93c69473d7
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-16 22:11:25 +0200

    working on the multiplier

--- JS/clicker.js
+++ JS/clicker.js
@@ -12,8 +12,28 @@ let axes = 0;
 let foxes = 0;
 let shotguns = 0
 
-//function that gets called when you click the chicken
+//function that gets called each time you click the chicken
 function increment() {
     chickens += 1;
     document.getElementById('showChickens').innerText = chickens;
+
+    if (chickens >= 7800000000) {
+        document.getElementById('gameOver').innerText = "You won the game! There will never be fried chicken again :'("
+    }
+}
+
+//function that happens every second
+setInterval(function (){
+    chickens += chickensPerSecond //adds the number of chickens per second to your total chickens
+
+    document.getElementById('showChickens').innerText = chickens;
+}, 1000)
+
+//will be called when user presses the buy button
+function buyAxe(){
+    if(chickens => axeCost){
+        chickens -= axeCost;
+        document.getElementById('showChickens').innerText = chickens;
+        axes++;
+    }
 }
