6a49cc50fce8a1451f175b3c7268f447d47bd606
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-17 09:53:05 +0200

    multiplier working

--- JS/clicker.js
+++ JS/clicker.js
@@ -1,20 +1,28 @@
+// all variables
 let chickens = 0;
 let chickensPerSecond = 0;
 let chickensPerClick = 1;
-let button = document.getElementById('clickButton');
-button.addEventListener("click", increment);
 
+// cost of items
 let axeCost = 10;
 let foxCost = 20;
 let shotCost = 200;
 
+// how many items you have
 let axes = 0;
 let foxes = 0;
 let shotguns = 0
 
+// Chicken incremen button
+const clicker = document.getElementById('clickButton');
+clicker.addEventListener("click", increment);
+
+const axeBuyBtn = document.getElementById('buyAxe');
+axeBuyBtn.addEventListener("click", buyAxe);
+
 //function that gets called each time you click the chicken
 function increment() {
-    chickens += 1;
+    chickens += chickensPerClick; //adds the chickens per click to your total chickens
     document.getElementById('showChickens').innerText = chickens;
 
     if (chickens >= 7800000000) {
@@ -29,11 +37,22 @@ setInterval(function (){
     document.getElementById('showChickens').innerText = chickens;
 }, 1000)
 
-//will be called when user presses the buy button
+//will be called when user presses the buy button for the axe
 function buyAxe(){
-    if(chickens => axeCost){
+    if(chickens >= axeCost){ //checks if you have enough chickens
+        //subtract cost of chickens
         chickens -= axeCost;
-        document.getElementById('showChickens').innerText = chickens;
+        document.getElementById('showChickens').innerText = chickens; //updates the HTML
+        //increments the amount of multipliers you have
         axes++;
+        document.getElementById('axeNum').innerText = axes;
+        // double the price
+        axeCost *= 2;
+        document.getElementById('axeCost').innerText = axeCost;
+        // multiply the number of chickens
+        chickensPerClick *= 2;
+        document.getElementById('showChickensPerClick').innerText = chickensPerClick;
+    } else {
+        alert("You don't have enough dead chickens!")
     }
 }
